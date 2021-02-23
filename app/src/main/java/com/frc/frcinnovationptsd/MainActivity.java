d7a63243a5ae5fb226a9e7dfce8d362a698ba523
Ziming <Zhuang412@pccsk12.com>
2021-02-22 23:33:08 -0500

    rename example folder

--- app/src/main/java/com/frc/frcinnovationptsd/MainActivity.java
+++ app/src/main/java/com/frc/frcinnovationptsd/MainActivity.java
@@ -1,6 +1,8 @@
-package com.example.frcinnovationptsd;
+package com.frc.frcinnovationptsd;
 
 import android.os.Bundle;
+
+import com.frc.frcinnovationptsd.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -13,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.frc.frcinnovationptsd.R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
--- app/src/main/java/com/frc/frcinnovationptsd/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/frc/frcinnovationptsd/ui/dashboard/DashboardFragment.java
@@ -1,4 +1,4 @@
-package com.example.frcinnovationptsd.ui.dashboard;
+package com.frc.frcinnovationptsd.ui.dashboard;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,7 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import com.example.frcinnovationptsd.R;
+import com.frc.frcinnovationptsd.R;
 
 public class DashboardFragment extends Fragment {
 
