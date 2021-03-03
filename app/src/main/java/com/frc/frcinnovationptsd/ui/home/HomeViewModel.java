b92fdde3d309c89a32c278df03f80b05decdf743
Ziming <Zhuang412@pccsk12.com>
2021-03-03 18:48:04 -0500

    incomplete home page draft

--- app/src/main/java/com/frc/frcinnovationptsd/ui/home/HomeViewModel.java
+++ app/src/main/java/com/frc/frcinnovationptsd/ui/home/HomeViewModel.java
@@ -6,15 +6,30 @@ import androidx.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<Integer> decibel;
+    private MutableLiveData<Integer> heartRate;
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        decibel = new MutableLiveData<>();
+        heartRate = new MutableLiveData<>();
+        decibel.setValue(0);
+        heartRate.setValue(0);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<Integer> getDecibel() {
+        return decibel;
+    }
+
+    public void setDecibel(Integer currentDecibel){
+        decibel.setValue(currentDecibel);
+    }
+
+    public LiveData<Integer> getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(Integer currentHeartRate){
+        heartRate.setValue(currentHeartRate);
     }
 
     // add data bindings
